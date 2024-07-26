9eef2cb2acd2ce5751eb7077441918e931481e1f
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2024-07-26 11:25:25 -0300

    Adicionei mais teclas

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 const teclasNum = [...document.querySelectorAll(".num")]
 const teclasOp = [...document.querySelectorAll(".op")]
 const teclaRes = document.querySelector("#res")
+const display = document.querySelector(".display")
+const ton = document.querySelector("#ton")
+const tlimpar = document.querySelector("#tlimpar")
 
+teclasNum.map((el)=>{
+    el.addEventListener("click",(evt)=>{
+        display.innerHTML += evt.target.innerHTML
+    })
+})
+teclasOp.map((el)=>{
+    el.addEventListener("click",(evt)=>{
+        display.innerHTML += evt.target.innerHTML
+    })
+})
+
+tlimpar.addEventListener("click",()=>{
+    display.innerHTML = "0"
+})
