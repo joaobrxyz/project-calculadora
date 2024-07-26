56b097a63965590d67a0a798d78efda4ce888e0e
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2024-07-26 12:20:01 -0300

    Atualizei o script
    Adicionei o tratamento de dados para não repetir os operadores de forma seguida

--- script.js
+++ script.js
@@ -4,18 +4,49 @@ const teclaRes = document.querySelector("#res")
 const display = document.querySelector(".display")
 const ton = document.querySelector("#ton")
 const tlimpar = document.querySelector("#tlimpar")
+const res = document.querySelector(".res")
+let sinal = false
+let decimal = false
 
 teclasNum.map((el)=>{
     el.addEventListener("click",(evt)=>{
-        display.innerHTML += evt.target.innerHTML
+        if(display.innerHTML == "0"){
+            display.innerHTML = ""
+        }
+        if (evt.target.innerHTML == ","){
+            if (display.innerHTML == ""){
+                display.innerHTML = "0"
+            }
+            if(!decimal){
+                decimal = true
+                sinal = true
+                display.innerHTML += evt.target.innerHTML
+            }
+        } else {
+            display.innerHTML += evt.target.innerHTML
+            sinal = false
+        }
     })
 })
 teclasOp.map((el)=>{
     el.addEventListener("click",(evt)=>{
-        display.innerHTML += evt.target.innerHTML
+        if (sinal === false){
+            if (display.innerHTML == "0"){
+                display.innerHTML = ""
+            }
+            if (evt.target.innerHTML == "x") {
+                display.innerHTML += "*"
+            } else {
+                display.innerHTML += evt.target.innerHTML
+            }
+            sinal = true
+            decimal = true
+        } 
     })
 })
 
 tlimpar.addEventListener("click",()=>{
     display.innerHTML = "0"
+    decimal = false
+    sinal = false
 })
